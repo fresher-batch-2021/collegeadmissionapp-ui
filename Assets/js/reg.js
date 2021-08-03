5fecf3d511913a0f08b14589acd1bfaa44313461
Manikandan S <manikandandeva22@gmail.com>
2021-08-03 17:58:53 +0530

    added images and registration ui pages developed

---
+++ Assets/js/reg.js
@@ -0,0 +1,51 @@
+function register() {
+    event.preventDefault();
+    // Get Form Values
+    const name = document.querySelector("#name").value;
+    if (name == "" || name == null || name.trim == "" || name == undefined) {
+        alert("Invalid Name");
+        return false;
+    }
+    const mobile = document.querySelector("#contact").value;
+    if (mobile == "" || mobile == null || mobile == undefined) {
+        alert("Invalid Contact Number");
+        return false;
+    }
+    const dob = document.querySelector("#dob").value;
+    if (dob == "") {
+        alert("Invalid DOB");
+        return false;
+    }
+    const email = document.querySelector("#email").value;
+    if (email == "" || email == null || email == undefined) {
+        alert("Invalid Email Address");
+        return false;
+    }
+    const password1 = document.querySelector("#pass1").value;
+    if (password1 == "") {
+        alert("Enter Password");
+        return false;
+    }
+    const password2 = document.querySelector("#pass2").value;
+    if (password1 == "") {
+        alert("Enter Password");
+        return false;
+    }
+    if (password1 != password2) {
+        alert("Invaid Password");
+        return false;
+    }
+    //console.log("Name :" + name + "-" + mobile + "-" + dob + "-" + email);
+    let userObj = {
+
+        "Name": name,
+        "Contact NO": mobile,
+        "DOB": dob,
+        "Email": email,
+        "Password1": password1,
+    };
+    console.log(userObj);
+
+    alert("Registration Successful");
+    window.location.href = "login.html";
+}
