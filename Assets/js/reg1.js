c3debbd5be7ac88226f2b8ccd3197a88dde372f9
Manikandan S <manikandandeva22@gmail.com>
2021-08-16 15:34:13 +0530

    cloudant1

--- Assets/js/reg1.js
+++ Assets/js/reg1.js
@@ -16,7 +16,11 @@ function register() {
             "regEmail": email
         };
 
-        let url = "https://product-mock-api.herokuapp.com/collegeadmissionapp/api/v1/auth/register";
+        const dbUserName = "apikey-v2-v1zh0zplguvn1ukyhpnqwpt7rhiuokz1bqggmlt9kw4";
+        const dbPassword = "163671d490ddeef138fc61e470881715";
+        const basicAuth = 'Basic ' + btoa(dbUserName + ':' + dbPassword);
+
+        let url = "https://21781b11-9dff-4242-9efa-fb21396540ca-bluemix.cloudantnosqldb.appdomain.cloud/collegeadmissionapp_user";
         let formData = {
             name: candidateName,
             username: userName,
@@ -33,7 +37,7 @@ function register() {
         Validator.isValidString(userName, "UserName Cannot be Blank");
         Password.isValidPassword(userPassword, "UserPassword contain atleast 8 Characters");
         Password.isValidPassword(confirmPassword, "ConfirmPassword contain atleast 8 Characters");
-        axios.post(url, formData).then(res => {
+        axios.post(url, formData, { headers: { 'Authorization': basicAuth } }).then(res => {
             let data = res.data;
             console.log(data);
             alert("Successffully Register");
