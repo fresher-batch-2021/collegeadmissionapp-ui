9493aec5d70a398b59be2ffc939fa27cc6263400
Manikandan S <manikandandeva22@gmail.com>
2021-08-16 17:11:55 +0530

    cloud2

--- Assets/js/login.js
+++ Assets/js/login.js
@@ -10,25 +10,36 @@ function login() {
             "password": password
         };
         console.log(userObj);
-        let url = "https://product-mock-api.herokuapp.com/collegeadmissionapp/api/v1/auth/login";
-        let formData = {
-            username: userName,
-            password: password
-        }
-        axios.post(url, formData).then(res => {
-            let data = res.data;
+        const dbUserName = "apikey-v2-v1zh0zplguvn1ukyhpnqwpt7rhiuokz1bqggmlt9kw4";
+        const dbPassword = "163671d490ddeef138fc61e470881715";
+        const basicAuth = 'Basic ' + btoa(dbUserName + ':' + dbPassword);
+
+        let url = "https://21781b11-9dff-4242-9efa-fb21396540ca-bluemix.cloudantnosqldb.appdomain.cloud/collegeadmissionapp_user/_find";
+        const selectedData = {
+            selector: {
+                username: userName,
+                password: password
+            },
+            fields: ["_id", "name", "contactNo", "email"]
+        };
+        axios.post(url, selectedData, { headers: { 'Authorization': basicAuth } }).then(res => {
+            let data = res.data.docs;
             console.log(data);
-            alert("Login Successful");
-            localStorage.setItem('userName', JSON.stringify(userObj));
-            window.location.href = "personalinfo.html";
-        }).catch(err => {
-            let errorMessage = err.response.data.errorMessage;
-            console.error(errorMessage);
-            alert(errorMessage);
-        });
+            if (data.length == 0) {
+                alert("Invalid Login Credentials");
+            }
+            else {
+                const user = data[0];
+                alert("Login Successful");
+                localStorage.setItem('userName', JSON.stringify(userObj));
+                window.location.href = "personalinfo.html";
+            }
+        }
+        )
 
     } catch (err) {
         console.error(err.message);
+        alert("Unable to Login");
         alert(err.message);
     }
 
